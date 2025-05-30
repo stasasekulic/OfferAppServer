b24520f13e9dc71f1a1d5952bfedfa59a7f7fac0
Staša Sekulić <stasa.sekulic.91@gmail.com>
2025-05-30 14:45:03 +0200

    Removed not used Optional

--- src/main/java/offerapp/user/UserRepository.java
+++ src/main/java/offerapp/user/UserRepository.java
@@ -2,8 +2,6 @@ package offerapp.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
     boolean existsByEmail(String email);
