2ac8f1475a6d12645a426a3e3d3eb97eac22a872
Staša Sekulić <stasa.sekulic.91@gmail.com>
2025-05-30 14:48:18 +0200

    Revert "Removed getters annd setters from UserResponse"
    This reverts commit f469d8dce9a0c918860009e486b890222531e518.

--- src/main/java/offerapp/user/dto/UserResponse.java
+++ src/main/java/offerapp/user/dto/UserResponse.java
@@ -4,13 +4,69 @@ import offerapp.user.User;
 import offerapp.user.enums.UserRole;
 
 public class UserResponse {
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private String phoneNumber;
+    private UserRole role;
 
     public UserResponse(User user) {
-        Long id = user.getId();
-        String name = user.getName();
-        String surname = user.getSurname();
-        String email = user.getEmail();
-        String phoneNumber = user.getPhoneNumber();
-        UserRole role = user.getRole();
+        this.id = user.getId();
+        this.name = user.getName();
+        this.surname = user.getSurname();
+        this.email = user.getEmail();
+        this.phoneNumber = user.getPhoneNumber();
+        this.role = user.getRole();
+    }
+
+    public UserResponse() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
     }
 }
