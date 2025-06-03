bc6b182a6f3afad6da56ce894cbe2cd1faf48871
Staša Sekulić <stasa.sekulic.91@gmail.com>
2025-06-03 13:00:11 +0200

    Added PDF generator

--- src/main/java/offerapp/offer/OfferController.java
+++ src/main/java/offerapp/offer/OfferController.java
@@ -2,7 +2,11 @@ package offerapp.offer;
 
 import offerapp.offer.dto.CreateOfferRequest;
 import offerapp.offer.dto.OfferResponse;
+import offerapp.offer.pdf.OfferGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,10 +17,11 @@ import java.util.List;
 public class OfferController {
 
     private final OfferService offerService;
-
+    private final OfferGenerator offerGenerator;
     @Autowired
-    public OfferController(OfferService offerService) {
+    public OfferController(OfferService offerService, OfferGenerator offerGenerator) {
         this.offerService = offerService;
+        this.offerGenerator = offerGenerator;
     }
 
     @PostMapping
@@ -38,5 +43,17 @@ public class OfferController {
     public ResponseEntity<Void> deleteOffer(@PathVariable Long id) {
         offerService.deleteOffer(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}/pdf")
+    public ResponseEntity<byte[]> generateOfferPdf(@PathVariable Long id) {
+        byte[] pdfBytes = offerGenerator.generatePdfForOffer(id);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        String filename = "offer_details_" + id + ".pdf";
+        headers.setContentDispositionFormData("attachment", filename);
+
+        return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
     }
 }
